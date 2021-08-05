d4780a1cb3820542a2f521e3822429a99eb014d8
Alec Campbell <88142969+22940dev@users.noreply.github.com>
2021-08-04 21:25:31 -0400

    Update socialLinks.ts

--- src/utils/social-links/socialLinks.ts
+++ src/utils/social-links/socialLinks.ts
@@ -1,9 +1,9 @@
-const facebook: string = 'https://www.facebook.com/profile.php?id=100013068189807'
-const linkedin: string = 'https://www.linkedin.com/in/mrluisfer/'
-const github: string = 'https://github.com/mrLuisFer'
-const twitter: string = 'https://twitter.com/lolesuncrak'
-const instagram: string = 'https://www.instagram.com/mrluisfer_/'
-const mailTo: string = 'mailto:lolesuncrak@gmail.com'
+const facebook: string = 'https://www.facebook.com/doc22940'
+const linkedin: string = 'https://www.linkedin.com/in/doc22940/'
+const github: string = 'https://github.com/22940dev'
+const twitter: string = 'https://twitter.com/Alec'
+const instagram: string = 'https://www.instagram.com/Alec/'
+const mailTo: string = 'mailto:freeunioncoder@gmail.com'
 
 // Separation to exports the social links
 export { facebook, linkedin, github, twitter, instagram, mailTo }
